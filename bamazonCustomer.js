447456b8837348d948b1b64d2f2d29b9a8034bc3
Austin Robbins <austinrobbins@Austins-MBP.hsd1.wa.comcast.net>
2019-08-17 23:40:22 -0700

    added comments

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
-
+// defines sql connection to bamazon_db
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
     password: "root",
     database: "bamazon_db",
 })
-
+// connects to db and displays db table
 connection.connect();
 function start(){
 let sql = `SELECT * FROM products`;
@@ -19,40 +19,40 @@ connection.query(sql, (err, res)=> {
     } else {
         console.table(res)
     } 
-    purchase(res);
+    purchase(res); // runs purchase with response as argument
 }) 
 };
 start();
 function purchase(response) {
     inquirer.prompt([
-        {
+        { //first user response
             message: 'what item (by ID) would you like to buy ?',
             name: 'id',
             type: 'input'
         },
-        {
+        { // second user response
             message: 'How many would you like to buy?',
             type: 'input',
             name: 'qty',
         },
     ]).then((res) => {
-        let index = res.id;
-        let itemID = parseInt(res.id) - 2;
-        let total = response[itemID].Stock - res.qty;
-        let cost = response[itemID].Price * res.qty;
+        let index = res.id; // set index to id
+        let itemID = parseInt(res.id) - 2; // sets appropriate index to id value
+        let total = response[itemID].Stock - res.qty; // grabs integer of stock in current products table and subtracts the number of items selected in the response
+        let cost = response[itemID].Price * res.qty; // multiplies the integer of price in the products table by the response quantity and displays so the user can see the total
         inventory(total, index, cost);
     });
 
 
     function inventory(stock,id, cost) {
-        if(stock <=0){
-            console.log('no stock');
-        }else{
+        if(stock <=0){ // check if out of stock
+            console.log('no stock'); 
+        }else{ // if not out of stock update total stock 
             connection.query(`UPDATE products SET Stock = ${stock} WHERE id = ${id}`, (err) =>{
                 if (err) throw err;
             });
-            console.log(`your total is $${cost}`);
+            console.log(`your total is $${cost}`); //display cost to user 
         }
-        setTimeout(start, 2000);
+        setTimeout(start, 2000); // repeat application over again for more sales!
     }
 }
